fb3eb8ac74ed98e5045817f221729e9156bed55b
Cephalonyx <cephalonyx@gmail.com>
2019-03-18 07:55:29 -0400

    First commit

---
+++ colors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#711c91',
+
+  primary: '#000000',
+  primaryLight: '#ae66df',
+  primaryDark: '#091833',
+
+  secondary: '#0abdc6',
+  secondaryLight: '#0abdc6',
+  secondaryDark: '#c60055',
+};
+
+//#711c91, #0abdc6, #091833
